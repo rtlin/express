7d2b8cafd6ff310afab242b96da9c7cdf4cac085
Robert Lin <rtlin99@gmail.com>
2017-11-15 18:01:45 -0500

    Completed the nodemon

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+/*
+var logger = function(req, res, next){
+	console.log('Logging...');
+	next();
+}
+
+app.use(logger);
+*/
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+var person = {
+		name: 'Jeff',
+		age: 30
+};
+
+app.get('/', function(req, res){
+	//res.send('Hello World - 3');
+	res.json(person);
+});
+
+app.listen(3000, function(){
+	console.log('Server started on Port 3000...');
+})
